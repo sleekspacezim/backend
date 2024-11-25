1b8d5912345559f8da2521570748504e17767f42
Mkhululi Ndlovu <87315888+mk870@users.noreply.github.com>
2024-11-25 13:01:35 +0200

    update

--- utilities/funcs/general/general.go
+++ utilities/funcs/general/general.go
@@ -1,15 +1,14 @@
 package general
 
 import (
-	"log"
+	//"log"
 	"math"
 
 	"math/rand"
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/joho/godotenv"
+	//"github.com/joho/godotenv"
 )
 
 func GenerateVerificationCode() int {
@@ -98,10 +97,10 @@ type EnvVariables struct {
 }
 
 func GetEnvVariables() EnvVariables {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatalf("Error loading .env file: %s", err)
+	// }
 	databaseDetails := os.Getenv("DATABASE_DETAILS")
 	email := os.Getenv("EMAIL")
 	emailPassword := os.Getenv("EMAIL_PASSWORD")
